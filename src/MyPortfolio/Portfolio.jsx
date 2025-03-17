96edd3f5c4a78d9e9c9d1e97b6f6112011805424
Mekala74 <Manimekala227@gmail.com>
2025-03-17 18:57:49 +0530

    New File Added

--- src/MyPortfolio/Portfolio.jsx
+++ src/MyPortfolio/Portfolio.jsx
@@ -151,7 +151,7 @@ const PortfolioPage = () => {
                                             {skill.icon}
                                         </div>
                                     </div>
-                                    <h3>{skill.title}</h3>
+                                    <h3 style={{ fontSize: "18px" }}>{skill.title}</h3>
                                 </div>
                             ))}
                         </div>
